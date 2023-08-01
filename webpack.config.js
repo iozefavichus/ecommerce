22d49fdcd5c9deb434d2682e87c0fcbe9539e4e1
iozefavichus <iozefavichus@gmail.com>
2023-08-01 15:27:02 +0200

    feat: add style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const EslingPlugin = require('eslint-webpack-plugin');
-const copyPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 const baseConfig = {
@@ -17,7 +17,7 @@ const baseConfig = {
             },
             {
                 test: /\.ts$/i,
-                use: "ts-loader",
+                use: 'ts-loader',
             },
             {
                 test: /\.html$/i,
@@ -40,17 +40,17 @@ const baseConfig = {
             filename: 'index.html',
         }),
         new EslingPlugin({ extensions: 'ts' }),
-        new copyPlugin({
-            patterns: [
-                { from: "src/images", to: "../dist/images" },
-            ]
+        new CopyPlugin({
+            patterns: [{ from: 'src/images', to: '../dist/images' }],
         }),
     ],
 };
 
 module.exports = ({ mode }) => {
     const isProductionMode = mode === 'prod';
-    const envConfig = isProductionMode ? require('./webpack.prod.config.js') : require('./webpack.dev.config');
+    const envConfig = isProductionMode
+        ? require('./webpack.prod.config.js')
+        : require('./webpack.dev.config');
 
     return merge(baseConfig, envConfig);
 };
