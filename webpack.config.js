546dae438e3b9923ff639ea0d4e4354b151a8c9f
dima92 <dima-tkachenko@mail.ru>
2023-08-02 20:49:00 +0300

    feat: add config for project

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,160 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable no-undef */
+const fs = require('fs');
 const path = require('path');
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const EslingPlugin = require('eslint-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const EslintPlugin = require('eslint-webpack-plugin');
 const ProdConfig = require('./webpack.prod.config');
 const DevConfig = require('./webpack.dev.config');
 
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all',
+    },
+  };
+
+  if (isProd) {
+    config.minimizer = [
+      new CssMinimizerPlugin({
+        test: /\.css$/i,
+      }),
+      new TerserPlugin({
+        test: /\.js(\?.*)?$/i,
+      }),
+    ];
+  }
+  return config;
+};
+
+const filename = (ext) => (isDev ? `[name].${ext}` : `[name].[contenthash:8].${ext}`);
+
+const cssLoaders = (extra) => {
+  const loaders = [
+    {
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        esModule: true,
+      },
+    },
+    'css-loader',
+  ];
+  if (extra) {
+    loaders.push(extra);
+  }
+  return loaders;
+};
+
+const pages = fs.readdirSync(path.resolve(__dirname, 'src')).filter((fileName) => fileName.endsWith('.html'));
+
+const plugins = () => {
+  return [
+    ...pages.map(
+      (page) =>
+        new HTMLWebpackPlugin({
+          template: page,
+          filename: page,
+          inject: 'body',
+          minify: {
+            collapseWhitespace: isProd,
+          },
+        }),
+    ),
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: '**/*',
+          context: path.resolve(__dirname, './src'),
+          globOptions: {
+            ignore: ['**/*.js', '**/*.ts', '**/*.css', '**/*.scss', '**/*.sass', '**/*.html'],
+          },
+          noErrorOnMissing: true,
+          force: true,
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename('css'),
+    }),
+    new EslintPlugin(),
+  ];
+};
+
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index'),
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.ts$/i,
-                use: 'ts-loader',
-            },
-            {
-                test: /\.html$/i,
-                use: 'html-loader',
-            },
-        ],
+  mode: 'development',
+  target: ['web', 'es6'],
+  entry: {
+    options: '@babel/polyfill',
+    main: './index.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: filename('js'),
+    chunkFilename: '[id].[chunkhash].js',
+    sourceMapFilename: '[file].map',
+    assetModuleFilename: '[file]',
+  },
+  performance: {
+    maxAssetSize: 2000000,
+    maxEntrypointSize: 2000000,
+  },
+  context: path.resolve(__dirname, 'src'),
+  resolve: {
+    extensions: ['.js', '.ts', '.json'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@types': path.resolve(__dirname, 'src/types'),
     },
-    resolve: {
-        extensions: ['', '.ts', '.js', '.css'],
-        plugins: [new TsconfigPathsPlugin()],
-        modules: ['node_modules'],
-    },
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, '../dist'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html'),
-            filename: 'index.html',
-        }),
-        new EslingPlugin({ extensions: 'ts' }),
-        new CopyPlugin({
-            patterns: [{ from: 'src/images', to: '../dist/images' }],
-        }),
+  },
+  optimization: optimization(),
+  devServer: {
+    hot: true,
+    port: 5000,
+  },
+  devtool: isDev ? 'source-map' : false,
+  plugins: plugins(),
+  module: {
+    rules: [
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg|webp)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(?:mp3|wav|ogg|mp4)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: cssLoaders(),
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: cssLoaders('sass-loader'),
+      },
     ],
+  },
 };
 
 module.exports = ({ mode }) => {
-    const isProductionMode = mode === 'prod';
-    const envConfig = isProductionMode ? ProdConfig : DevConfig;
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode ? ProdConfig : DevConfig;
 
-    return merge(baseConfig, envConfig);
+  return merge(baseConfig, envConfig);
 };
