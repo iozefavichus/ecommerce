cd6e0e29ab7c7a95d2b57713f2c35289f5cb573d
dima92 <dima-tkachenko@mail.ru>
2023-09-05 19:27:35 +0300

    feat: add 2 card for about page

--- src/components/pages/about/draw-about.ts
+++ src/components/pages/about/draw-about.ts
@@ -23,9 +23,28 @@ const createAboutPage = (): HTMLElement => {
   a1.setAttribute('target', '_blank');
   const imgGithub1 = createCustomElement('img', ['social-media-icon']) as HTMLImageElement;
   imgGithub1.src = './assets/images/about/github.png';
+  const cellBlock2 = createCustomElement('div', [
+    'responsive-cell-block',
+    'desk-4',
+    'ipadp-4',
+    'mobile-12',
+    'tab-12',
+    'card-container',
+  ]);
+  const name2 = createCustomElement('p', ['text', 'name'], 'Konstantin Matveev');
+  const position2 = createCustomElement('p', ['text', 'position'], 'Developer');
+  const img2 = createCustomElement('img', ['team-member-image']) as HTMLImageElement;
+  img2.src = './assets/images/about/kostya.jpg';
+  const a2 = createCustomElement('a', ['']) as HTMLLinkElement;
+  a2.href = 'https://github.com/mat-kon';
+  a2.setAttribute('target', '_blank');
+  const imgGithub2 = createCustomElement('img', ['social-media-icon']) as HTMLImageElement;
+  imgGithub2.src = './assets/images/about/github.png';
   a1.append(imgGithub1);
+  a2.append(imgGithub2);
   cellBlock1.append(name1, position1, img1, a1);
-  cardContainer.append(cellBlock1);
+  cellBlock2.append(name2, position2, img2, a2);
+  cardContainer.append(cellBlock1, cellBlock2);
   inner.append(heading, subheading, cardContainer);
   wrapper.append(inner);
   return wrapper;
