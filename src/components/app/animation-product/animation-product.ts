6879ecc4d6a3605b3f9e51d9a1778a2eb125f368
Mat-Kon <mat_kon@mail.ru>
2023-09-11 19:17:31 +0300

    feat: add animation add product in cart

---
+++ src/components/app/animation-product/animation-product.ts
@@ -0,0 +1,19 @@
+import { createCustomElement } from '../../shared/utilities/helper-functions';
+
+const animationProductInCart = (event: MouseEvent) => {
+  const parentElem = (event.target as HTMLElement).parentElement;
+  const cartBtn = createCustomElement('button', ['cart-btn', 'animation']) as HTMLButtonElement;
+  const x = event.offsetX;
+  let y = event.offsetY;
+  cartBtn.style.right = `${x}px`;
+  cartBtn.style.bottom = `100px`;
+  parentElem?.append(cartBtn);
+  const animation = setTimeout(() => {
+    cartBtn.style.bottom = `${(y += 600)}px`;
+  }, 0);
+  const removeCartBtn = setTimeout(() => {
+    parentElem?.removeChild(cartBtn);
+  }, 2000);
+};
+
+export { animationProductInCart };
